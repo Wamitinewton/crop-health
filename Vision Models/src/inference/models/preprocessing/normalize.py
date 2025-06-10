b3e11b80b85b330e0c9c26189245091ccb702a6e
Rebecca Okoko <okokorebecca39@gmail.com>
2025-06-10 14:37:44 +0300

    Delete Vision Models/src/inference/models directory

--- Vision Models/src/inference/models/preprocessing/normalize.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-def get_resize_and_rescale(image_size):
-    """Returns a Sequential layer for resizing and rescaling"""
-    return tf.keras.Sequential([
-        tf.keras.layers.Resizing(image_size, image_size),
-        tf.keras.layers.Rescaling(1./255)
-    ])
